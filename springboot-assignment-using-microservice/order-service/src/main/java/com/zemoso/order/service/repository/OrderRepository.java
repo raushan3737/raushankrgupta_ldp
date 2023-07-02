562818e6a7d386ac6e40fc0a91e7b930750cc26b
Raushan Kumar Gupta <raushan.gupta@zemosolabs.com>
2023-07-03 00:57:19 +0530

    Cleaning workspace

--- springboot-assignment-using-microservice/order-service/src/main/java/com/zemoso/order/service/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.zemoso.order.service.repository;
-
-import com.zemoso.order.service.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- springboot-assignment-using-microservice/user-service/src/main/java/com/zemoso/user/service/exception/CustomException.java
+++
@@ -1,8 +0,0 @@
-package com.zemoso.user.service.exception;
-
-
-public class CustomException extends RuntimeException {
-    public CustomException(String message) {
-        super(message);
-    }
-}
